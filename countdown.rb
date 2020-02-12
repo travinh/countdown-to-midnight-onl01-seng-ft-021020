c7020ce7ad60780f386ad088b0855c64f7c80333
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-12 23:12:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,4 +9,11 @@ def countdown (number)
 end
 
 
-countdown_with_sleep
+def countdown_with_sleep
+  while (number >0)
+    puts "#{number} SECOND(S)!"
+    number-=1
+  end
+  puts "HAPPY NEW YEAR!"
+  
+end
